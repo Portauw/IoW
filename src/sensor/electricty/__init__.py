bcd8e6bf021689ba900842a9bc008ad4af52f650
Michael <bouchmi@cronos.be>
2021-12-22 23:42:54 +0100

    wrap duct with lists to pass to sensors

--- src/sensor/electricty/__init__.py
+++ src/sensor/electricty/__init__.py
@@ -8,14 +8,14 @@ import grovepi
 
 
 class ACSensor(Process, EdgiseBase):
-    def __init__(self, stop_event: Event, logging_q: Queue, input_q: Queue, output_q: Queue, config_json, **kwargs):
+    def __init__(self, stop_event: Event, logging_q: Queue, input_q: Queue, output_q: Queue, config_json:str, **kwargs):
         self._stop_event = stop_event
         self._logging_q: Queue = logging_q
         self._input_q: Queue = input_q
         self._output_q: Queue = output_q
         self._output_q: Queue = output_q
         self.RMS_voltage = 230
-        self._config = config_json
+        self._config_json: str = config_json
         self.info("{} -  type {}".format(self._config, type(self._config)))
         # for key, val in kwargs.items():
         #     self.info("key: {} - value: {}".format(key,val))
@@ -47,8 +47,8 @@ class ACSensor(Process, EdgiseBase):
 
     def run(self) -> None:
         self.info("Starting AC sensor")
-        print(type(self._config))
-        config = json.loads(self._config)
+        print(type(self._config_json))
+        config = json.loads(self._config_json)
 
         for k,v in config:
             print(v)
