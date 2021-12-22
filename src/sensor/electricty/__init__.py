4b6e93fff7d9d250ef38cc237cc36681425c2bd8
Michael <bouchmi@cronos.be>
2021-12-22 23:13:49 +0100

    wrap duct with lists to pass to sensors

--- src/sensor/electricty/__init__.py
+++ src/sensor/electricty/__init__.py
@@ -14,7 +14,7 @@ class ACSensor(Process, EdgiseBase):
         self._output_q: Queue = output_q
         self.RMS_voltage = 230
         self._config = config_list
-        self.info("{} -  type {}".format(self._config, type(config)))
+        self.info("{} -  type {}".format(self._config, type(self._config)))
         # for key, val in kwargs.items():
         #     self.info("key: {} - value: {}".format(key,val))
         #     setattr(self, key, val)
