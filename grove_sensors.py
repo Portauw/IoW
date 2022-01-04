144ece28bdc84419a910b9d60f3004b8f6fae1fa
Michael <bouchmi@cronos.be>
2022-01-04 22:49:55 +0100

    add seperate script for sensors to execute in python3.5

---
+++ grove_sensors.py
@@ -0,0 +1,75 @@
+import grovepi
+
+# AC Sensor
+AC_sensor_config = {
+    'name': "Electricity sensor",
+    'pin': 0,
+    'type': "INPUT",
+    'unit': "mA",
+}
+grovepi.pinMode(AC_sensor_config['pin'], AC_sensor_config['type'])
+
+
+def read_ac_sensor():
+    sensor_value = grovepi.analogRead(AC_sensor_config['pin'])
+    return sensor_value
+
+
+def calc_amplitude_current(sensor_value):
+    return float(sensor_value / 1024 * self.VCC / 800 * 2000000)
+
+
+def calc_RMS_current(amplitude_current):
+    return amplitude_current / sqrt(2)
+
+
+def calc_avg_power_consumption(RMS_current):
+    return RMS_voltage * RMS_current
+
+
+def measure_AC():
+    raw_val = read_ac_sensor()
+    print("Raw Value: {}".format(raw_val))
+    amplitude_current = calc_amplitude_current(raw_val)
+    print("A I Value: {}".format(amplitude_current))
+    rms_current = calc_RMS_current(amplitude_current)
+    print("RMS I Value: {}".format(rms_current))
+    avg_power = calc_avg_power_consumption(rms_current)
+    print("AVG W Value: {}".format(avg_power))
+
+    measurement = {
+        'RawVal': raw_val,
+        'CurrentAmp': amplitude_current,
+        'RMSCurrent': rms_current,
+        'AVGPower': avg_power
+    }
+    return measurement
+
+
+# Vibration sensor
+
+# connect vibration sensor to analog port A2
+vibration_sensor_config = {
+    'name': 'Vibration sensor',
+    'pin': 2,
+    'type': 'INPUT',
+    'unit': 'MHz',
+}
+grovepi.pinMode(vibration_sensor_config['pin'], vibration_sensor_config['type'])
+
+
+def read_sensor():
+    sensor_value = grovepi.analogRead(vibration_sensor_config['pin'])
+    return sensor_value
+
+
+def measure_vibration():
+    self.info("Starting vibration sensor")
+
+    raw_val = self.read_sensor()
+    print("Raw Value Vibration: {}".format(raw_val))
+
+    measurement = {
+        'RawVal': raw_val,
+    }
+    return measurement
